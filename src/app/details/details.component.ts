84a5d18c1d6e04473406eb6569a69ed73d7ea0a5
adriana ruiz <adriannaruiz24@gmail.com>
2024-07-14 16:25:13 -0500

    feat: add form on detail page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {HousingService} from '../housing.service';
 import {Housinglocation} from '../housinglocation';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
@@ -15,8 +16,22 @@ export class DetailsComponent {
   housingService = inject(HousingService);
   housingLocation: Housinglocation | undefined;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
   constructor() {
    const housingLocationId = Number(this.route.snapshot.params['id'])
    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId)
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
